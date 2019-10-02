7bb0f9c5b721d2e7f5071e2bcfb3e53e907317f2
Kamil Rocki <kmrocki@MacBook-Pro-3.local>
2019-10-02 09:56:40 -0700

    points

--- gui/points.c
+++ gui/points.c
@@ -3,11 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <math.h>
+
 #define GL_SILENCE_DEPRECATION 1
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+
+#define MAX_POINTS 32
+int points_len = 0;
+struct point {
+  int x;
+  int y;
+} points[MAX_POINTS];
+
+void insert_point(int x, int y) {
+  if (points_len < MAX_POINTS) {
+    points[points_len++] = (struct point){ .x=x, .y=y };
+  }
+}
+
+void remove_point() {
+  if (points_len > 0) {
+    points_len--;
+  }
+}
 
 void *work(void *args);
 
@@ -19,8 +40,6 @@ void *work(void *args);
 #include <GLFW/glfw3.h>
 #define AUTO_REFRESH 60
 #define OFFSET 64
-#define WIDTH 512
-#define HEIGHT 512
 #define IM_W 256
 #define IM_H 256
 
@@ -33,6 +52,9 @@ u32 cyclelimit = 0;
 u8 debugmode = 0;
 u8 show_debug = 0;
 u8 pix[3*IM_W*IM_H];
+
+int WIDTH = 512;
+int HEIGHT = 512;
 
 double t0; // global start time
 double get_time() {
@@ -56,10 +78,15 @@ int nsleep(long ms) {
 #define bind_key(x,y) \
 { if (action == GLFW_PRESS && key == (x)) (y) = 1; if (action == GLFW_RELEASE && key == (x)) (y) = 0; if (y) {printf(#y "\n");} }
 
+static void change_size_callback(GLFWwindow* window, int width, int height)
+{
+  WIDTH = width;
+  HEIGHT = height;
+}
+
 static void error_callback(int error, const char* description) { }
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (action == GLFW_PRESS && key == GLFW_KEY_0) show_debug ^= 1;
-    if (action == GLFW_PRESS && key == GLFW_KEY_9) limit_speed ^= 1;
+    if (action == GLFW_PRESS && key == GLFW_KEY_U) remove_point();
     if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE) glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
@@ -72,14 +99,17 @@ static void mouse_pos_callback(GLFWwindow* window, double x, double y) {
   glfwGetWindowSize(window, &width, &height);
   mx = (int)(((double)IM_W*x)/(double)WIDTH);
   my = (int)(-0.5+((double)IM_H*y)/(double)HEIGHT);
-  printf("(%d, %d)\n", mx, my);
+  //printf("(%d, %d)\n", mx, my);
 }
 
 static void mouse_btn_callback(GLFWwindow* window, int button, int action, int mods) {
   if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
     double xpos, ypos;
     glfwGetCursorPos(window, &xpos, &ypos);
-    printf("click at (x=%f, y=%f)\n", xpos, ypos);
+    int x = (int)(((double)IM_W*xpos)/(double)WIDTH);
+    int y = (int)(-0.5+((double)IM_H*ypos)/(double)HEIGHT);
+    //printf("click at x=%.3f (%d), y=%.3f (%d)\n", xpos, x, ypos, y);
+    insert_point(x, y);
   }
 }
 
@@ -99,8 +129,35 @@ static GLFWwindow* open_window(const char* title, GLFWwindow* share, int posX, i
     glfwSetKeyCallback(window, key_callback);
     glfwSetMouseButtonCallback(window, mouse_btn_callback);
     glfwSetCursorPosCallback(window, mouse_pos_callback);
+    glfwSetWindowSizeCallback(window, change_size_callback);
 
     return window;
+}
+
+void draw_point(int x, int y, float r, float g, float b, float a) {
+
+    float incr_x = 1.0f/(float)IM_W; float incr_y = 1.0f/(float)IM_H;
+    glColor4f(r, g, b, a);
+    float i = x * incr_x;
+    float j = y * incr_y;
+    j = flip_y ? 1-j : j; // FLIP vert
+    glVertex2f(i,      j     );     glVertex2f(i+incr_x, j     );
+    glVertex2f(i+incr_x, j+incr_y); glVertex2f(i,      j+incr_y);
+}
+
+void draw_line(int x0, int y0, int x1, int y1, float r, float g, float b, float a) {
+
+    glLineWidth(1.0);
+    float incr_x = 1.0f/(float)IM_W; float incr_y = 1.0f/(float)IM_H;
+    glColor4f(r, g, b, a);
+    float i0 = x0 * incr_x;
+    float j0 = y0 * incr_y;
+    float i1 = x1 * incr_x;
+    float j1 = y1 * incr_y;
+    j0 = flip_y ? 1-j0 : j0; // FLIP vert
+    j1 = flip_y ? 1-j1 : j1; // FLIP vert
+    glVertex2f(i0, j0 );
+    glVertex2f(i1, j1 );
 }
 
 static void draw_quad()
@@ -110,25 +167,21 @@ static void draw_quad()
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    float incr_x = 1.0f/(float)IM_W; float incr_y = 1.0f/(float)IM_H;
     glOrtho(0.f, 1.f, 0.f, 1.f, 0.f, 1.f);
     glBegin(GL_QUADS);
-    float i,j;
-    u8 px,py;
-    for (u16 x=0; x<IM_W; x++) for (u16 y=0; y<IM_H; y++ ) {
-         i = x * incr_x; j = y * incr_y; px = x; py = flip_y ? IM_H - y - 1 : y; // FLIP vert
 
-         if (px == mx & py == my) {
-           glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
-         } else {
-          glColor4f(pix[3*(px+py*IM_W)+0]/255.0f,
-                    pix[3*(px+py*IM_W)+1]/255.0f,
-                    pix[3*(px+py*IM_W)+2]/255.0f,
-                    255.0f/255.0f);
-         }
-          glVertex2f(i,      j     );     glVertex2f(i+incr_x, j     );
-          glVertex2f(i+incr_x, j+incr_y); glVertex2f(i,      j+incr_y);
-    }
+    for (int i = 0; i < points_len; ++i)
+      draw_point(points[i].x, points[i].y, 0, 1, 0, 0.75);
+
+    // cursor
+    draw_point(mx, my, 1, 0, 0, 0.5);
+    glEnd();
+
+    glBegin(GL_LINES);
+
+    for (int i = 0; i < points_len; ++i)
+      draw_line(mx, my, points[i].x, points[i].y, 0, 0, 1, 0.75);
+
     glEnd();
 };
 
@@ -198,28 +251,16 @@ void *work(void *args) {
 
   int i = 0;
 
-  u8 arr[65536];
-  FILE *f = fopen((const char*)args, "rb");
-  if (!f) fprintf(stderr, "couldn't open %s\n", (const char*)args);
-  else {
-    fread(arr, 65536, 1, f);
-    for (int i = 0; i < 65536; i++) {
-      pix[3*i+0] = arr[i];
-      pix[3*i+1] = arr[i];
-      pix[3*i+2] = arr[i];
-    }
-    fclose(f);
-  }
-
   while (!gl_ok) nsleep(10);
   {
     while (gl_ok) {
-      //printf("working %d\n", i++);
-      nsleep(1000);
-      if (my >= 0 && mx >= 0 && mx < 256 && my < 256) {
-        printf("%d, %d is %04x, val = %04x\n", my, mx, mx + 256 * my, arr[mx + 256 * my]);
-      }
-      //pix[i % (IM_W * IM_H * 3)] = rand() % 256;
+      nsleep(100);
+      //for (int i = 0; i < points_len; ++i) {
+      //  int x = points[i].x;
+      //  int y = points[i].y;
+      //  double d = sqrt( (mx-x)*(mx-x) + (my-y)*(my-y)  );
+      //  printf("%3d: x=%3d, y=%3d, d=%3.3f\n", i, mx, my, d);
+      //}
     }
 
   }
